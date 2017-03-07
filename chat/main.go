6e4cd409c64241ea01450a3fed123ffc88b0f79d
Bruno Almeida <bruno@disvolvi.com>
2017-03-07 18:13:24 -0300

    Host can send the reply message

--- chat/main.go
+++ chat/main.go
@@ -27,12 +27,28 @@ func RunHost(ip string) {
 	}
 
 	fmt.Println("New Connection Accepted")
+
+	for {
+		handleHost(conn)
+	}
+}
+
+func handleHost(conn net.Conn) {
 	reader := bufio.NewReader(conn)
 	message, readErr := reader.ReadString('\n')
 	if readErr != nil {
 		log.Fatal("Error: ", readErr)
 	}
 	fmt.Println("Message received: ", message)
+
+	fmt.Print("Send message: ")
+	replyReader := bufio.NewReader(os.Stdin)
+	replyMessage, replyErr := replyReader.ReadString('\n')
+	if replyErr != nil {
+		log.Fatal("Error: ", replyErr)
+	}
+
+	fmt.Fprint(conn, replyMessage)
 }
 
 // RunGuest takes an ip as argument

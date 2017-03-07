6e13e1fb1376b0b5e294644e10f0b5dd3ec139bd
Bruno Almeida <bruno@disvolvi.com>
2017-03-07 17:46:43 -0300

    Loggin

--- chat/main.go
+++ chat/main.go
@@ -12,16 +12,20 @@ const port = "8080"
 // RunHost takes an ip as argument
 // and listen for connection
 func RunHost(ip string) {
-	listener, listenErr := net.Listen("tcp", ip+":"+port)
+	ipAndPort := ip + ":" + port
+
+	listener, listenErr := net.Listen("tcp", ipAndPort)
 	if listenErr != nil {
 		log.Fatal("Error: ", listenErr)
 	}
+	fmt.Println("Listen to: ", ipAndPort)
 
 	conn, acceptError := listener.Accept()
 	if acceptError != nil {
 		log.Fatal("Error: ", acceptError)
 	}
 
+	fmt.Println("New Connection Accepted")
 	reader := bufio.NewReader(conn)
 	message, readErr := reader.ReadString('\n')
 	if readErr != nil {
